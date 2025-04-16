9b96f50d00dbcc05fb0683e409f97f34b5098da9
Lancerxp <165858738+Lancerxp@users.noreply.github.com>
2025-04-16 08:25:04 -0500

    busqueda

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Importa CommonModule para *ngFor y pipes
-import { FormsModule } from '@angular/forms'; // Importa FormsModule para [(ngModel)]
-import { RouterModule } from '@angular/router'; 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BloonService, Bloon } from '../../services/bloon.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl:'./home.component.html',
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  standalone: true, // Marca el componente como standalone
-  imports: [CommonModule, FormsModule, RouterModule, HttpClientModule] // Agrega los módulos necesarios
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule, HttpClientModule]
 })
 export class HomeComponent implements OnInit {
-  productosFiltrados: Bloon[] = [];
+  productos: Bloon[] = []; // Array original con todos los productos
+  productosFiltrados: Bloon[] = []; // Array para los resultados filtrados
   terminoBusqueda = '';
 
   constructor(private productoService: BloonService) {}
@@ -24,15 +25,21 @@ export class HomeComponent implements OnInit {
 
   obtenerProductos(): void {
     this.productoService.getAll().subscribe((productos) => {
-      this.productosFiltrados = productos;
+      this.productos = productos; // Guarda todos los productos en el array original
+      this.productosFiltrados = productos; // Inicializa los productos filtrados con todos los productos
     });
   }
 
   buscar(): void {
     const termino = this.terminoBusqueda.toLowerCase();
-    this.productosFiltrados = this.productosFiltrados.filter(producto =>
+    this.productosFiltrados = this.productos.filter(producto =>
       producto.descripcion_del_codigo.toLowerCase().includes(termino)
     );
+  }
+
+  resetBusqueda(): void {
+    this.productosFiltrados = [...this.productos]; // Copia todos los productos al array filtrado
+    this.terminoBusqueda = ''; // Limpia el término de búsqueda
   }
 
   agregarProducto(): void {
